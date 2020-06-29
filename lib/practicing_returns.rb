138c1bf1c2e815498dc07c0f4e195804621fa755
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 21:53:57 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -7,6 +7,17 @@ def hello(array)
     i += 1
   end
 end
+require 'pry'
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+binding.pry
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
